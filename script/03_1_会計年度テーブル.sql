1b794a504fb802205e78f7042dc4074ef69368f8
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2023-04-26 02:55:37 +0900

    パズル1回答

--- script/03_1_会計年度テーブル.sql
+++ script/03_1_会計年度テーブル.sql
@@ -2,14 +2,28 @@
 
 -- 米国政府の会計年度(10/1-9/30)のテーブル
 -- 本ではテーブル名はキャメルケースだけど、このリポジトリではPostgreSQLを使ってるのでスネークケースにした。
-CREATE TABLE  hogeschema.fiscal_year_table1 (
-  fiscal_year INT, -- 会計年度
-  start_date DATE, -- 年度開始日
-  end_date DATE    -- 年度終了日
+CREATE TABLE hogeschema.fiscal_year_table1
+(
+    fiscal_year INT  NOT NULL PRIMARY KEY, -- 会計年度
+    start_date  DATE NOT NULL,             -- 年度開始日。開始日は年次の1年前の10/1しか許可しない。
+    CONSTRAINT valid_start_date CHECK (
+            (EXTRACT(YEAR FROM start_date) = fiscal_year - 1) -- EXTRACT関数は第一引数で指定した数値を返す
+            AND (EXTRACT(MONTH FROM start_date) = 10)
+            AND (EXTRACT(DAY FROM start_date) = 01)
+        ),
+    end_date    DATE NOT NULL,             -- 年度終了日。終了日は年次と同年の9/30しか許可しない。
+    CONSTRAINT valid_end_date CHECK (
+            (EXTRACT(YEAR FROM end_date) = fiscal_year)
+            AND (EXTRACT(MONTH FROM end_date) = 09)
+            AND (EXTRACT(DAY FROM end_date) = 30)
+        )
 );
 
 GRANT ALL PRIVILEGES ON hogeschema.fiscal_year_table1 TO hoge;
 
-INSERT INTO hogeschema.fiscal_year_table1 VALUES('1', '2000-10-01', '2001-09-30');
-INSERT INTO hogeschema.fiscal_year_table1 VALUES('2', '2000-10-01', '2001-09-30');
-INSERT INTO hogeschema.fiscal_year_table1 VALUES('3', '2000-10-01', '2001-09-30');
+INSERT INTO hogeschema.fiscal_year_table1
+VALUES (2001, '2000-10-01', '2001-09-30');
+INSERT INTO hogeschema.fiscal_year_table1
+VALUES (2002, '2001-10-01', '2002-09-30');
+INSERT INTO hogeschema.fiscal_year_table1
+VALUES (2003, '2002-10-01', '2003-09-30');
